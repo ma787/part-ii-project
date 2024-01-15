b06a5f46791fe749acafa327fcb47c3c4468883d
Mohamed <49460356+ma787@users.noreply.github.com>
2024-01-15 02:50:18 +0000

    Update iozone_tests.sh

--- all_tests/tests/iozone_tests.sh
+++ all_tests/tests/iozone_tests.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+PDIR="${CWD%/*}"
+
+cd $PDIR
+
 mkdir -p iozoneout
 for i in {1..100}
 do
 	if [ $i -lt 10 ]
 	then
-		iozone -a /dev/sda1 > iozone-00$((i)).out
+		iozone -a $1 > iozone-00$((i)).out
 		cp iozone-00$((i)).out iozoneout
 	elif [ $i -lt 100 ]
 	then
-		iozone -a /dev/sda1 > iozone-0$((i)).out
+		iozone -a $1 > iozone-0$((i)).out
 		cp iozone-0$((i)).out iozoneout
 	else
-		iozone -a /dev/sda1 > iozone-$((i)).out
+		iozone -a $1 > iozone-$((i)).out
 		cp iozone-$((i)).out iozoneout
 	fi
 done
 ls iozoneout | xargs > f.txt
-cat f.txt | xargs python3 /home/mohamed/Project/all_tests/iozone2csv.py
+cat f.txt | xargs python3 ./iozone2csv.py
 rm f.txt
 rm -rf iozoneout
 for i in {1..100}
@@ -25,7 +30,7 @@ do
 	if [ $i -lt 10 ]
 	then
 		rm iozone-00$((i)).out
-	elif [ $i -lt 10 ]
+	elif [ $i -lt 100 ]
 	then
 		rm iozone-0$((i)).out
 	else
