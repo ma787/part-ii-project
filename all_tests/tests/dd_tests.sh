91490afe9f990e262ebbbd3cb3ecb0af28c61e40
Mohamed <49460356+ma787@users.noreply.github.com>
2024-01-15 02:49:18 +0000

    Update dd_tests.sh

--- all_tests/tests/dd_tests.sh
+++ all_tests/tests/dd_tests.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+PDIR="${CWD%/*}"
+
+cd $CWD
+
 for i in {1..100}
 do
-	for i in 7 8 9 10
+	for j in 7 8 9 10
 	do
 		sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-		dd if=/dev/zero of=/mnt/speetest bs=$((2**i*1024)) count=1024 conv=fdatasync 2>o.txt
-    	/home/mohamed/Project/all_tests/tests/get_dd_throughput.sh -i ./o.txt -o "dd_writes.csv"
+		dd if=/dev/zero of=/mnt/speetest bs=$((2**j*1024)) count=1024 conv=fdatasync 2>o.txt
+    	./get_dd_throughput.sh -i ./o.txt -o "dd_writes.csv"
     done
 done
 
 for i in {1..100}
 do
-	for i in 7 8 9 10
+	for j in 7 8 9 10
 	do
     	sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-    	dd if=/mnt/speetest of=/dev/null bs=$((2**i*1024)) count=1024 2>o.txt
-    	/home/mohamed/Project/all_tests/tests/get_dd_throughput.sh -i ./o.txt -o "dd_reads.csv"
+    	dd if=/mnt/speetest of=/dev/null bs=$((2**j*1024)) count=1024 2>o.txt
+    	./get_dd_throughput.sh -i ./o.txt -o "dd_reads.csv"
     done
 done
 
@@ -24,3 +29,5 @@ rm ./o.txt
 rm /mnt/speetest
 chmod a+rw ./dd_writes.csv
 chmod a+rw ./dd_reads.csv
+
+cd -
