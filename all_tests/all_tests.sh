d443fbe7ae6e559a978a538c2f1dbed90919a7cb
Mohamed <49460356+ma787@users.noreply.github.com>
2024-01-15 02:48:42 +0000

    Update all_tests.sh

--- all_tests/all_tests.sh
+++ all_tests/all_tests.sh
@@ -3,11 +3,11 @@
 mkdir -p results
 
 ./tests/dd_tests.sh
-./tests/hdparm_tests.sh
-./tests/iozone_tests.sh
+./tests/hdparm_tests.sh $1
+./tests/iozone_tests.sh $1
 
-mv dd_reads.csv results
-mv dd_writes.csv results
-mv hdparm_buffered.csv results
-mv hdparm_cached.csv results
+mv ./tests/dd_reads.csv results
+mv ./tests/dd_writes.csv results
+mv ./tests/hdparm_buffered.csv results
+mv ./tests/hdparm_cached.csv results
 mv iozone.csv results
