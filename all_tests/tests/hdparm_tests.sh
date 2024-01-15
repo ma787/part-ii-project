fa729325d2c7c02638f585662d11295ac568f598
Mohamed <49460356+ma787@users.noreply.github.com>
2024-01-15 02:49:57 +0000

    Update hdparm_tests.sh

--- all_tests/tests/hdparm_tests.sh
+++ all_tests/tests/hdparm_tests.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+
+CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+PDIR="${CWD%/*}"
+
+cd $CWD
+
 for i in {1..100}
 do
-    hdparm -tT /dev/sda1 > o.txt
-    /home/mohamed/Project/all_tests/tests/get_hdparm_throughput.sh -i ./o.txt -c "hdparm_cached.csv" -b "hdparm_buffered.csv"
+    hdparm -tT $1 > o.txt
+    ./get_hdparm_throughput.sh -i ./o.txt -c "hdparm_cached.csv" -b "hdparm_buffered.csv"
 done
 
 rm ./o.txt
 chmod a+rw ./hdparm_cached.csv
 chmod a+rw ./hdparm_buffered.csv
+
+cd -
