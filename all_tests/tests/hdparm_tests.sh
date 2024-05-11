846c0d611de0c9231d0ed94680d290b5f775ba33
Mohamed <49460356+ma787@users.noreply.github.com>
2024-05-11 05:24:06 +0100

    Update hdparm_tests.sh

--- all_tests/tests/hdparm_tests.sh
+++ all_tests/tests/hdparm_tests.sh
@@ -5,10 +5,11 @@ PDIR="${CWD%/*}"
 
 cd $CWD
 
-for i in {1..100}
+for i in $(seq "$2") 
 do
     hdparm -tT $1 > o.txt
     ./get_hdparm_throughput.sh -i ./o.txt -c "hdparm_cached.csv" -b "hdparm_buffered.csv"
+    echo "hdparm run $i out of $2 completed"
 done
 
 rm ./o.txt
