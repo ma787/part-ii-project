5fcebb7089392ec6384d2dad56440bd945704d73
Mohamed <49460356+ma787@users.noreply.github.com>
2024-05-11 01:48:31 +0100

    Update dd_tests.sh

--- all_tests/tests/dd_tests.sh
+++ all_tests/tests/dd_tests.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
 CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
-PDIR="${CWD%/*}"
 
-cd $CWD
+cd "$CWD" || exit
 
-for i in {1..100}
+for i in $(seq "$1") 
 do
-	for j in 7 8 9 10
+for j in 7 8 9 10
 	do
 		sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-		dd if=/dev/zero of=/mnt/speetest bs=$((2**j*1024)) count=1024 conv=fdatasync 2>o.txt
+		dd if=/dev/zero of="$2"/null_data bs=$((2**j*1024)) count=1024 conv=fdatasync 2>o.txt
+		echo "dd write test run $i out of $1 completed"
     	./get_dd_throughput.sh -i ./o.txt -o "dd_writes.csv"
     done
+    i=$((i+1))
 done
 
-for i in {1..100}
+for i in $(seq "$1") 
 do
 	for j in 7 8 9 10
 	do
     	sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-    	dd if=/mnt/speetest of=/dev/null bs=$((2**j*1024)) count=1024 2>o.txt
+    	dd if="$2"/null_data of=/dev/null bs=$((2**j*1024)) count=1024 2>o.txt
+    	echo "dd write test run $i out of $1 completed"
     	./get_dd_throughput.sh -i ./o.txt -o "dd_reads.csv"
     done
+    i=$((i+1))
 done
 
 rm ./o.txt
-rm /mnt/speetest
+rm "$2"/null_data
 chmod a+rw ./dd_writes.csv
 chmod a+rw ./dd_reads.csv
 
-cd -
+cd - || exit
