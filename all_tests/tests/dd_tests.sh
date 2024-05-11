844fe7201b5746bf1f2be97e7dd205fc86cc2000
Mohamed <49460356+ma787@users.noreply.github.com>
2024-05-11 14:33:45 +0100

    Update dd_tests.sh

--- all_tests/tests/dd_tests.sh
+++ all_tests/tests/dd_tests.sh
@@ -8,8 +8,8 @@ for i in $(seq "$1")
 do
 for j in 7 8 9 10
 	do
-		sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-		dd if=/dev/zero of="$2"/null_data bs=$((2**j*1024)) count=1024 conv=fdatasync 2>o.txt
+		sh -c "sync && echo 3 > /proc/sys/vm/drop_caches" # clears the memory cache
+		dd if=/dev/zero of="$2"/null_data bs=1M count=$(2**j) conv=fdatasync 2>o.txt
 		echo "dd write test run $i out of $1 completed"
     	./get_dd_throughput.sh -i ./o.txt -o "dd_writes.csv"
     done
@@ -20,7 +20,7 @@ do
 	for j in 7 8 9 10
 	do
     	sh -c "sync && echo 3 > /proc/sys/vm/drop_caches"
-    	dd if="$2"/null_data of=/dev/null bs=$((2**j*1024)) count=1024 2>o.txt
+    	dd if="$2"/null_data of=/dev/null bs=1M count=$(2**j) 2>o.txt
     	echo "dd write test run $i out of $1 completed"
     	./get_dd_throughput.sh -i ./o.txt -o "dd_reads.csv"
     done
