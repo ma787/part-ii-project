2de9ff4d1d0543704f5acd47b4cd5286ad7bfa92
Mohamed <49460356+ma787@users.noreply.github.com>
2024-05-11 01:49:07 +0100

    Update get_dd_throughput.sh

--- all_tests/tests/get_dd_throughput.sh
+++ all_tests/tests/get_dd_throughput.sh
@@ -12,10 +12,18 @@ def main():
     
     with open(args.i, "r") as f:
     	t_out_arr = f.readlines()
+    print(t_out_arr)
     t_out_result = t_out_arr[2].split(" ")
     
     with open(args.o, "a") as g:
-    	g.write("{},".format(t_out_result[-2]))
+        res = int(t_out_result[-2])
+        unit = t_out_result[-1][:2]
+        sec = t_out_result[-4]
+        if unit == "GB":
+            res *= 1000
+        else if unit == "KB":
+            res /= 1000
+    	g.write("{}:{},".format(res, sec))
 
 if __name__ == "__main__":
     try:
